28223040197d24aa51d6bfdefa085e9839e18dca
monarthur <fsjogren@gmail.com>
2016-10-15 14:24:48 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-/* tree shaking: https://medium.com/@jonnysamps/angular-2-bundling-with-rollup-4738d0148a2c#.hsold1xr6 */
+/* tree shaking: https://medium.com/@jonnysamps/angular-2-bundling-with-rollup-4738d0148a2c#.hsold1xr6 
+lazy loading: https://medium.com/@daviddentoom/angular-2-lazy-loading-with-webpack-d25fe71c29c1#.doav8pf26 */
 
 import 'zone.js';
 import 'reflect-metadata';
